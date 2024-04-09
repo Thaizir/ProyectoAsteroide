75401662b3034ef49836bbaf1d9988aec603d179
thaizire@gmail.com <thaizire@gmail.com>
2024-04-08 19:32:29 -0500

    solved issue

--- Frontend/proyecto_asteroide/src/components/Asteroids.jsx
+++ Frontend/proyecto_asteroide/src/components/Asteroids.jsx
@@ -12,51 +12,51 @@ export const Asteroids = ({ startDate, endDate }) => {
         setSelectedAsteroid(asteroid);
     };
 
-    const fetchAsteroids = async (value_1, value_2) => {
-        try {
-            let response;
-            if (value_1 === value_2) {
-                response = await fetch(`http://localhost:3000/?date=${startDate}`);
-                console.log(response)
-            } else {
-                response = await fetch(`http://localhost:3000/range?start_date=${startDate}&end_date=${endDate}`);
-            }
-        } catch (err) {
-            console.log(err)
-        }
-    }
+
 
     useEffect(() => {
 
-        fetchAsteroids(startDate, endDate)
-            .then(response => response.json())
-            .then(data => console.log(data.json()))
-            .then(asteroid => setAsteroids(asteroid))
-            .then(() => setIsPending(false))
-            .catch(err => console.log(err));
+        async function fetchAsteroids(value_1, value_2) {
+            console.log(value_1, value_2)
+            try {
+                let response;
+                if (value_1 === value_2) {
+                    response = await fetch(`http://localhost:3000/?date=${startDate}`);
+                } else {
+                    response = await fetch(`http://localhost:3000/range?start_date=${startDate}&end_date=${endDate}`);
+                }
+                const data = await response.json();
+                setAsteroids(data);
+                setIsPending(false);
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        fetchAsteroids(startDate, endDate);
+
     }, [startDate, endDate]);
 
-    console.log(asteroids);
+
 
     return (
         <>
-
             {isPending && <p>Loading...</p>}
-            {
-                asteroids.map(ast => (
-                    <div key={asteroid.name}>
-                        <button onClick={() => handleAsteroidClick(ast)}>Name: {ast.name}</button>
-                        {selectedAsteroid === ast && (
-                            <div>
-                                <p>{ast.hazardous ? "Hazardous" : "Not Hazardous"}</p>
-                                <p>Min diameter: {ast.diameter.estimated_diameter_min}</p>
-                                <p>Max diameter: {ast.diameter.estimated_diameter_max}</p>
-                            </div>
-                        )}
-                    </div>
-                ))
-            }
+            {asteroids.map((ast) => (
+                <div key={ast.name} className="asteroids-container">
 
+                    <div className="asteroid" onClick={() => handleAsteroidClick(ast)}>Name: {ast.name}</div>
+                    {selectedAsteroid === ast && (
+                        <div className="asteroid" >
+                            <p>{ast.name}</p>
+                            <p>{ast.hazardous ? "Hazardous" : "Not Hazardous"}</p>
+                            <p>Min diameter: {ast.diameter.estimated_diameter_min}</p>
+                            <p>Max diameter: {ast.diameter.estimated_diameter_max}</p>
+                        </div>
+                    )}
+
+                </div>
+            ))}
         </>
-    )
+    );
 };
